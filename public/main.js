347d31240e6ba943aa4e2a9d76833f773895c5eb
Allan <allan@Allans-MBP.localdomain>
2018-09-11 19:01:10 -0400

    scoreboard

--- public/main.js
+++ public/main.js
@@ -1,5 +1,55 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+let score = 0;
+const addOneToTeamOneScore = event => {
+  score += 1;
+  /*let total = `${score}`;*/
+  /*console.log(total);*/
+  let message = `${score}`;
+  const team1Score = document.querySelector(".team1 .score");
+  team1Score.textContent = message;
+};
 
-document.addEventListener('DOMContentLoaded', main)
+const subOneFromTeamOneScore = event => {
+  score -= 1;
+  /*let total = `${score}`;*/
+  /*console.log(total);*/
+  let message = `${score}`;
+  const team1Score = document.querySelector(".team1 .score");
+  team1Score.textContent = message;
+};
+const subOneFromTeamTwoScore = event => {
+  score -= 1;
+  /*let total = `${score}`;*/
+  /*console.log(total);*/
+  let message = `${score}`;
+  const team1Score = document.querySelector(".team2 .score");
+  team1Score.textContent = message;
+};
+
+const addOneToTeamTwoScore = event => {
+  score += 1;
+  let total = `${score}`;
+  /*console.log(total);*/
+  let message = `${score}`;
+  const team1Score = document.querySelector(".team2 .score");
+  team1Score.textContent = message;
+};
+
+const main = event => {
+  const addButton = document.querySelector(".team1 .add");
+
+  addButton.addEventListener("click", addOneToTeamOneScore);
+
+  const subButton = document.querySelector(".team1 .sub");
+
+  subButton.addEventListener("click", subOneFromTeamOneScore);
+
+  const subButton2 = document.querySelector(".team2 .sub");
+
+  subButton2.addEventListener("click", subOneFromTeamTwoScore);
+
+  const addButton2 = document.querySelector(".team2 .add");
+
+  addButton2.addEventListener("click", addOneToTeamTwoScore);
+};
+
+document.addEventListener("DOMContentLoaded", main);
